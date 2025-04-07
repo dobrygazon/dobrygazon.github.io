91e8beb0062801594ab214865898f14933bebdb2
Ramil Gasanov <gasanoff.ramil@gmail.com>
2025-04-07 12:49:19 +0400

    Added GA4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import partytown from "@astrojs/partytown";
+
 export default defineConfig({
   site: 'https://dobrygazon.github.io',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), partytown()],
 });
